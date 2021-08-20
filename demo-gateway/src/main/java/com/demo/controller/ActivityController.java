d3250603de9b4360510ed3d75e2d701478ba6df7
wangzhangfei5 <wangzhangfei5@jd.com>
2021-08-20 20:54:21 +0800

    解决跨域问题

--- demo-gateway/src/main/java/com/demo/controller/ActivityController.java
+++ demo-gateway/src/main/java/com/demo/controller/ActivityController.java
@@ -14,6 +14,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.Date;
 
 @Controller
+@CrossOrigin
 @RequestMapping( "/activity" )
 public class ActivityController {
 
